e3fa847263e2ed3901ca93a46b1afbc1df656e49
Subroutine7901 <77363873+Subroutine7901@users.noreply.github.com>
2022-11-23 18:51:25 -0700

    Stuff

---
+++ defs.py
@@ -0,0 +1,11 @@
+def menu():
+    return f
+    """
+    Welcome adventurer to the world of Rayloft:tm:!
+
+    1. New Game
+    2. Load Game
+    3. Settings
+    4. Input Code
+    5. Quit
+    """
